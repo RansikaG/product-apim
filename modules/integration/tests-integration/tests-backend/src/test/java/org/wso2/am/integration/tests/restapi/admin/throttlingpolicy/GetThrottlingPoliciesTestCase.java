14417d3287a01ead1bbf8a2957e99cd1ebb3455c
RansikaG <ransika1998@gmail.com>
2022-06-03 16:13:27 +0530

    changing get policies query

--- modules/integration/tests-integration/tests-backend/src/test/java/org/wso2/am/integration/tests/restapi/admin/throttlingpolicy/GetThrottlingPoliciesTestCase.java
+++ modules/integration/tests-integration/tests-backend/src/test/java/org/wso2/am/integration/tests/restapi/admin/throttlingpolicy/GetThrottlingPoliciesTestCase.java
@@ -1,39 +1,41 @@
+/*
+ *
+ *   Copyright (c) 2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *   WSO2 Inc. licenses this file to you under the Apache License,
+ *   Version 2.0 (the "License"); you may not use this file except
+ *   in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing,
+ *   software distributed under the License is distributed on an
+ *   "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *   KIND, either express or implied.  See the License for the
+ *   specific language governing permissions and limitations
+ *   under the License.
+ *
+ */
+
 package org.wso2.am.integration.tests.restapi.admin.throttlingpolicy;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.common.io.Files;
-import com.google.gson.Gson;
 import org.apache.http.HttpStatus;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Factory;
 import org.testng.annotations.Test;
-import org.wso2.am.integration.clients.admin.api.dto.AdvancedThrottlePolicyDTO;
-import org.wso2.am.integration.clients.admin.api.dto.ExportThrottlePolicyDTO;
 import org.wso2.am.integration.test.helpers.AdminApiTestHelper;
-import org.wso2.am.integration.test.impl.DtoFactory;
-import org.wso2.am.integration.test.utils.APIManagerIntegrationTestException;
 import org.wso2.am.integration.test.utils.base.APIMIntegrationBaseTest;
 import org.wso2.am.integration.test.utils.base.APIMIntegrationConstants;
 import org.wso2.carbon.automation.engine.context.TestUserMode;
 import org.wso2.carbon.integration.common.admin.client.UserManagementClient;
-
-import java.io.File;
-import java.io.IOException;
 import java.net.URL;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Paths;
-import java.util.stream.Stream;
-
 import static org.wso2.am.integration.test.utils.generic.APIMTestCaseUtils.encodeCredentials;
 
 public class GetThrottlingPoliciesTestCase extends APIMIntegrationBaseTest {
@@ -45,7 +47,8 @@ public class GetThrottlingPoliciesTestCase extends APIMIntegrationBaseTest {
 
     private String getThrottlePoliciesUrl;
 
-    @Factory(dataProvider = "userModeDataProvider") public GetThrottlingPoliciesTestCase(TestUserMode userMode) {
+    @Factory(dataProvider = "userModeDataProvider")
+    public GetThrottlingPoliciesTestCase(TestUserMode userMode) {
         this.userMode = userMode;
     }
 
@@ -64,26 +67,21 @@ public class GetThrottlingPoliciesTestCase extends APIMIntegrationBaseTest {
 
         getThrottlePoliciesUrl =
                 adminURLHttps + APIMIntegrationConstants.REST_API_ADMIN_CONTEXT_FULL_0 + getThrottlePoliciesResource;
-
     }
 
     @Test(groups = { "wso2.am" }, description = "Exported Sample ThrottlePolicy with endpoint security enabled")
     public void testThrottlePolicyExport() throws Exception {
-
         try (CloseableHttpClient client = HttpClientBuilder.create().build()) {
         //construct get Throttle Policies url
         URL exportRequest =
-                new URL(getThrottlePoliciesUrl+"?query=type:all" );
+                new URL(getThrottlePoliciesUrl);
         HttpGet request = new HttpGet(exportRequest.toURI());
-
         request.setHeader("Content-Type", "application/json");
         request.setHeader("Accept", "application/json");
         request.setHeader(APIMIntegrationConstants.AUTHORIZATION_HEADER,
                 "Basic " + encodeCredentials(user.getUserName(),user.getPassword().toCharArray()));
-
         CloseableHttpResponse response =  client.execute(request);
         Assert.assertEquals(response.getStatusLine().getStatusCode(), HttpStatus.SC_OK);
         }
-
     }
 }
